1d306c80e72c807b65b0c5cfa2246b7587ff9ed6
Lucas Jesus <cc19372@g.unicamp.br>
2020-08-10 08:04:25 -0300

    add a constructor on PostTO

--- ng/post-web/src/app/models/post.ts
+++ ng/post-web/src/app/models/post.ts
@@ -1,7 +1,13 @@
-export interface Post {
+export class Post {
     id: string;
     autor: string;
     titulo: string;
     texto: string;
     data: string;
+
+    constructor(autor: string, titulo: string, texto: string) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.texto = texto;
+    }
 }
