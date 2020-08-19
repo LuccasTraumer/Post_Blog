f8aca9e83f3f1ae0f3a5c3252fb21ae17b4de1f0
Lucas Jesus <cc19372@g.unicamp.br>
2020-08-19 10:12:27 -0300

    validação na remoação de um Post e alteração no TO do Post angular

--- ng/post-web/src/app/add-post/add-post.component.ts
+++ ng/post-web/src/app/add-post/add-post.component.ts
@@ -37,8 +37,25 @@ export class AddPostComponent implements OnInit, AfterViewInit {
     }
   }
 
-  editarPost(post: Post) {
-    this.service.editarPost(post);
+  editarPost($event) {
+    let newPost: Post;
+    if (this.hasEditPost) {
+      newPost = this.service.getEditPost();
+    }
+    console.log(newPost.id);
+    const autor = (<HTMLInputElement>document.getElementById('txtAutor')).value;
+    const titulo = (<HTMLInputElement>document.getElementById('txtTitulo')).value;
+    const texto = (<HTMLInputElement>document.getElementById('txtTexto')).value;
+
+    if (autor !== undefined || titulo !== undefined || texto !== undefined) {
+      newPost.setAutor(newPost, autor);
+      newPost.setTitulo(newPost, titulo);
+      newPost.setTexto(newPost, texto);
+      
+      console.log(newPost.id);
+    }
+    console.log(newPost.id);
+    this.service.editarPost(newPost);
     return;
   }
 
@@ -46,26 +63,25 @@ export class AddPostComponent implements OnInit, AfterViewInit {
     const post = this.service.getEditPost();
     let temEditPost = false;
     if (post !== undefined) {
-      console.log(post.autor);
-      const autor = (<HTMLInputElement>document.getElementById('txtAutor'));
-      const titulo = (<HTMLInputElement>document.getElementById('txtTitulo'));
-      const texto = (<HTMLInputElement>document.getElementById('txtTexto'));
-      autor.setAttribute('value', post.autor);
-      titulo.setAttribute('value', post.titulo);
-      texto.value = post.texto;
+      this.preencherInputs(post);
       temEditPost = true;
       return temEditPost;
     }
     return temEditPost;
   }
 
+  preencherInputs(post: Post) {
+    const autor = (<HTMLInputElement>document.getElementById('txtAutor'));
+    const titulo = (<HTMLInputElement>document.getElementById('txtTitulo'));
+    const texto = (<HTMLInputElement>document.getElementById('txtTexto'));
+    autor.setAttribute('value', post.autor);
+    titulo.setAttribute('value', post.titulo);
+    texto.value = post.texto;
+  }
+
   savePost(autor: string, titulo: string, texto: string): void {
     this.post = new Post(autor, titulo, texto);
     this.service.salvarPost(this.post).subscribe(() => {});
     this.service.pegarTodosPosts();
-  }
-
-  getPutPost(post: Post) {
-    console.log('Get Put Post: ' + post);
   }
 }
