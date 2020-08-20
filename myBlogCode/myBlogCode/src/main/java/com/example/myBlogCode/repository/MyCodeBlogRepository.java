e8df6778629bc8d38b02c860b0d9195ed2b51c6b
Lucas Jesus <cc19372@g.unicamp.br>
2020-08-20 07:43:04 -0300

    remove folder not used

--- myBlogCode/myBlogCode/src/main/java/com/example/myBlogCode/repository/MyCodeBlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.myBlogCode.repository;
-
-import com.example.myBlogCode.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-@CrossOrigin()
-public interface MyCodeBlogRepository extends MongoRepository<Post, String> {
-}
--- post/src/main/java/com/code/post/repository/BlogPostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.code.post.repository;
-
-import com.code.post.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogPostRepository extends JpaRepository<Post, Long> {
-}
